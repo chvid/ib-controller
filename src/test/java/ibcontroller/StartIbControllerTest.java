b4478f88798a920a660432d8a012930536560b3b
Christian Hvid <christian.hvid@gmail.com>
2018-09-09 21:23:23 +0200

    Fix of test case.

--- src/test/java/ibcontroller/StartIbControllerTest.java
+++ src/test/java/ibcontroller/StartIbControllerTest.java
@@ -1,6 +1,5 @@
 package ibcontroller;
 
-import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -8,20 +7,11 @@ import java.io.File;
 
 @Ignore
 public class StartIbControllerTest {
-    /*
-    @Before
-    public void setup() {
-        System.setProperty("sun.java2d.noddraw", "true");
-        System.setProperty("swing.boldMetal", "false");
-        System.setProperty("sun.locale.formatasdefault", "true");
-    }
-    */
-
     @Test
     public void startIbControler() throws Exception {
         System.setProperty("user.dir", new File("work").getAbsolutePath());
         System.out.println(System.getProperty("user.dir"));
-        IBController.main(new String[] { "resources/IBController.ini" });
+        IBController.main(new String[] { "src/main/etc/IBController.ini" });
         Thread.sleep(60000);
     }
 }
